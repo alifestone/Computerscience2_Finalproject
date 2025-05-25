21cf15f19b98631ccc6322dfb0238a71f52c9232
Reo <catatangerine@gmail.com>
2025-05-26 00:02:55 +0900

    Update draft.c

--- draft.c
+++ draft.c
@@ -1,72 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include<stdint.h>
-#include<stdbool.h>
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_ttf.h>
-#include<SDL2/SDL2_gfxPrimitives.h>
-#define MAX_CARD 50
-#define WCARD 100
-#define HCARD 140
-#define WSCREEN 1200
-#define HSCREEN 800
-#define HAND_SIZE 5
-typedef enum{
-    CARD_ATK,           //ATK = attack
-    CARD_DEF,           //DEF = defense
-    CARD_TEC,           //TEC = tech
-    CARD_SPE            //SPE = special
-} Type;
-typedef struct{
-    char name[50];
-    Type type;
-    int32_t cst;        // energy cost
-    int32_t dmg;        // for attack
-    int32_t blk;        // for defense
-    int32_t mov;        // lane movement
-    int32_t rng;        // 0 = melee , 1 = adjacent, 2 = global
-    void (*effect)(void *self,void *target);
-} Card;
-typedef struct{
-    Card *cards[MAX_CARD];
-    int32_t cnt;        //cnt
-} Deck;
-typedef struct{
-    char name[50];
-    int32_t health;
-    int32_t energy;
-    int32_t lane;       //0, 1, or 2
-    Deck base;
-    Deck skilltree1;
-    Deck skilltree2;
-    Deck skilltree3;
-} Fable;
-typedef struct{
-    Fable *fable;
-    Deck draw;          //抽牌
-    Deck disc;          //墓地
-    Deck bani;          //除外
-    Deck hand;          //手牌
-} Player;
-
-typedef struct{
+#include "fables.h"
+#include "base.h"
+#include "declare.h"
+#include "draw.h"
+#include "cards.h"
+typedef struct {
     const char *txt;
     int32_t x,y;
     int32_t a;
     bool activate;
 } Floating;
 
-bool Playing=0;
+bool Playing = 0;
 
 //-------------------- 介面 --------------------
 void grid(SDL_Renderer *ren){
     SDL_SetRenderDrawColor(ren, 30, 30, 60, 255);
-    for (int x = 0; x <= WSCREEN; x += 100) {
+    for (int x = 0; x <= WSCREEN; x += 120) {
         SDL_RenderDrawLine(ren, x, 0, x, HSCREEN);
     }
-    for (int y = 0; y <= HSCREEN; y += 100) {
+    for (int y = 0; y <= HSCREEN; y += 120) {
         SDL_RenderDrawLine(ren, 0, y, WSCREEN, y);
     }
 }
@@ -110,7 +62,7 @@ void TitleScreen(SDL_Renderer *ren,TTF_Font *font){
             }
             if(e.type==SDL_MOUSEBUTTONDOWN){
                 int32_t mx=e.button.x, my=e.button.y;
-                if(mx>=400&&mx<=800&&my>=550&&my<=650){
+                if(mx>=XCENTER-200&&mx<=XCENTER+200&&my>=YCENTER+60&&my<=YCENTER+160){
                     intitle=0;
                 }
             }
@@ -118,17 +70,42 @@ void TitleScreen(SDL_Renderer *ren,TTF_Font *font){
         SDL_SetRenderDrawColor(ren,255,255,255,255);
         SDL_RenderClear(ren);
         grid(ren);
-        SDL_Rect box={600,600,400,100};
+        SDL_Rect box={XCENTER,YCENTER+110,400,100};
         DrawButton(ren,box,r,btnclr,font,txtclr,"Play 1v1");
 
+        //battle_setup(NULL,NULL,ren);
         SDL_RenderPresent(ren);
         SDL_Delay(16);
+    }
+}
+int randomize_round(SDL_Renderer *ren, TTF_Font *font) {
+    int dice_roll = rand() % 6 + 1; // Simulate a dice roll (1-6)
+    SDL_Color txtclr = {0, 0, 0, 255};
+    char message[50];
+
+    if (dice_roll % 2 == 1) {
+        snprintf(message, sizeof(message), "P1 goes first! (Dice: %d)", dice_roll);
+        WriteText(ren, message, font, txtclr, WSCREEN / 2, HSCREEN / 2 - 50);
+        return 1; // P1 goes first
+    } else {
+        snprintf(message, sizeof(message), "P2 goes first! (Dice: %d)", dice_roll);
+        WriteText(ren, message, font, txtclr, WSCREEN / 2, HSCREEN / 2 - 50);
+        return 2; // P2 goes first
     }
 }
 void BattleScreen(SDL_Renderer *ren,Player *p1,Player *p2,TTF_Font *font){
     bool inbattle=1;
     bool fighting=1;
     SDL_Event e;
+
+    // Randomize who goes first
+    SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+    SDL_RenderClear(ren);
+    grid(ren);
+    int first_player = randomize_round(ren, font);
+    SDL_RenderPresent(ren);
+    SDL_Delay(2000); // Pause to show the result
+    battle_setup(p1,p2,1);
     while(inbattle){
         while(SDL_PollEvent(&e)){
             if(e.type==SDL_QUIT){
@@ -161,8 +138,12 @@ void BattleScreen(SDL_Renderer *ren,Player *p1,Player *p2,TTF_Font *font){
                 inbattle=0;
             }
         }
+
         SDL_SetRenderDrawColor(ren,255,255,255,255);
         SDL_RenderClear(ren);
+        grid(ren);
+        BattleLane(ren,p1,p2);
+        //printf("%d,%d\n",p1->draw.cnt,p1->hand.cnt);
         for(int i=0;i<p1->hand.cnt;i++){
             SDL_Rect rect={50+i*(WCARD+20),HSCREEN-HCARD+40,WCARD,HCARD};
             SDL_SetRenderDrawColor(ren,200,0,0,255);
@@ -170,127 +151,119 @@ void BattleScreen(SDL_Renderer *ren,Player *p1,Player *p2,TTF_Font *font){
             SDL_SetRenderDrawColor(ren,0,0,0,255);
             SDL_RenderDrawRect(ren,&rect);
         }
-        grid(ren);
-        SDL_Rect hp1={50,(HCARD-40)/2,p2->fable->health*4,20};
-        SDL_Rect hp2={WSCREEN-(p1->fable->health*4+50),HSCREEN-(HCARD-40)/2,p1->fable->health*4,20};
-        SDL_SetRenderDrawColor(ren,0,200,0,255);
-        SDL_RenderFillRect(ren,&hp1);
-        SDL_RenderFillRect(ren,&hp2);
-        SDL_Color txtclr={0,0,0,255};
         
-        if(p1->fable->health==0&&p2->fable->health==0){
-            WriteText(ren,"Draw",font,txtclr,WSCREEN/2,HSCREEN/2);
-            fighting=0;
-        }else if(p1->fable->health==0){
-            WriteText(ren,"P2 wins",font,txtclr,WSCREEN/2,HSCREEN/2);
-            fighting=0;
-        }else if(p2->fable->health==0){
-            WriteText(ren,"P1 wins",font,txtclr,WSCREEN/2,HSCREEN/2);
-            fighting=0;
-        }
         SDL_RenderPresent(ren);
         SDL_Delay(15);
     }
 }
-//-------------------- 機制 --------------------
-void add_deck(Deck *deck, Card *card){
-    if(deck->cnt<MAX_CARD)deck->cards[deck->cnt++]=card;
-}
-Card *draw_deck(Deck *deck){
-    if(deck->cnt==0)return NULL;
-    return deck->cards[--deck->cnt];
-}
-void shuffle_deck(Deck *deck){
-    for(int i=deck->cnt-1;i>0;i--){
-        int32_t j=rand()%(i+1);
-        Card *tmp=deck->cards[i];
-        deck->cards[i]=deck->cards[j];
-        deck->cards[j]=tmp;
+void BattleLane(SDL_Renderer *ren,Player *p1, Player *p2){
+    for(int i=-4;i<=4;i++){
+        SDL_Rect rect={XCENTER+i*(WCARD+20),YCENTER,WCARD,HCARD};
+        SDL_Color relic={0,0,0,255};
+        DrawRect(ren,rect,10,relic);
     }
+    filledCircleRGBA(ren,XCENTER-(WCARD+20),YCENTER,40,p1->fable->Piece.r,p1->fable->Piece.g,p1->fable->Piece.b,p1->fable->Piece.a);
 }
-void draw_hand(Player *player){
-    for(int i=0;i<HAND_SIZE;i++){
-        Card *card=draw_deck(&player->draw);
-        if(card)add_deck(&player->hand,card);
-    }
-}
-//-------------------- 效果 --------------------
-void redhood_effect(void *self, void *target){
-    Player *atker=(Player *)self;
-    Player *defer=(Player *)target;
-    if(atker->fable->lane==defer->fable->lane){
-        defer->fable->health-=4;
-        printf("[%s hits %s with Red Slash for 4 damage.]\n",atker->fable->name,defer->fable->name);
-    }else{
-        printf("[%s's Red Slash missed.\n]",atker->fable->name);
-    }
-}
-//-------------------- 角色 --------------------
-Card red_hood={
-    .name="Red Riding Hood",
-    .type=CARD_ATK,
-    .cst=1,
-    .dmg=4,
-    .blk=0,
-    .mov=0,
-    .rng=0,
-    .effect=redhood_effect
+//-------------------- Set up-s --------------------
+Deck general_shop={
+    .cards={&Attack1,&Defense1,&Move1,&Wild1,&Attack2,&Defense2,&Move2,&Attack3,&Defense3,&Move3},
+    .cnt=10
 };
-void setup_red(Player *p){
-    Fable *red=malloc(sizeof(Fable));
-    *red=(Fable){
-        .health=40,
-        .energy=2,
-        .lane=1
+void fable_shop_setup(Fable *fable){
+    FableShop shop={
+        .Attack={
+            .cards=memcpy(malloc(sizeof(Card*)*5),fable->skill[0].cards,sizeof(Card*)*5),
+            .cnt=5
+        },
+        .Defense={
+            .cards=memcpy(malloc(sizeof(Card*)*5),fable->skill[1].cards,sizeof(Card*)*5),
+            .cnt=5
+        },
+        .Move={
+            .cards=memcpy(malloc(sizeof(Card*)*5),fable->skill[2].cards,sizeof(Card*)*5),
+            .cnt=5
+        }
     };
-    strcpy(red->name,"Red Riding Hood");
-    p->fable=red;
-    add_deck(&p->draw,&red_hood);
-    add_deck(&p->draw,&red_hood);
-    add_deck(&p->draw,&red_hood);
-    add_deck(&p->draw,&red_hood);
-    add_deck(&p->draw,&red_hood);
-    shuffle_deck(&p->draw);
-    draw_hand(p);
 }
-//-------------------- 模擬 --------------------
-int main(){
+void starting_deck(Player *p){
+    for(int i=0;i<3;i++){
+        add_deck(&p->draw,&Attack1);
+        add_deck(&p->draw,&Defense1);
+        add_deck(&p->draw,&Move1);
+    }
+    add_deck(&p->draw,p->fable->skill[0].cards[0]); // Pot Shot
+    add_deck(&p->draw,p->fable->skill[1].cards[0]); // Energy Shield
+    add_deck(&p->draw,p->fable->skill[2].cards[0]); // Unleashed Ballistics
+    printf("%d\n",p->draw.cnt);
+    shuffle_deck(&p->draw);   
+}
+void setup_player(Player *p, Fable *fable) {
+    p->fable = fable;
+}
+void battle_setup(Player *p1, Player *p2,int starting){
+    setup_player(p1, &red_hood);
+    starting_deck(p1);
+    //setup_player(p2, &red_hood);
+    //strcpy(p2->fable->name, "Dummy");
+    //filledCircleRGBA(ren,XCENTER+(WCARD+20),YCENTER,40,p2->fable->Piece.r,p2->fable->Piece.g,p2->fable->Piece.b,p2->fable->Piece.a);
+    if(starting==1){
+        draw_hand(p1,4);
+        printf("P1's hand: %d cards\n", p1->hand.cnt);
+        /*for(int i=0;i<6;i++){
+            draw_hand(p2);
+        }*/
+    }/*else if(starting==2){
+        for(int i=0;i<6;i++){
+            draw_hand(p1);
+        }
+        for(int i=0;i<4;i++){
+            draw_hand(p2);
+        }
+    }*/
+}
+
+//-------------------- 主程式 --------------------
+int main() {
     srand(time(NULL));
-    Playing=1;
-    Player p1={0}, p2={0};
+    Playing = 1;
+    Player p1 = {0}, p2 = {0};
 
     // Font Initialization
     TTF_Init();
-    TTF_Font *font=TTF_OpenFont("NotoSans-Regular.ttf",30);
-    if(!font){
-        printf("Failed to load font: %s\n",TTF_GetError());
+    TTF_Font *font = TTF_OpenFont("NotoSans-Regular.ttf", 30);
+    if(!font) {
+        printf("Failed to load font: %s\n", TTF_GetError());
         return 1;
     }
-    //-----------------------------
-    setup_red(&p1); setup_red(&p2);
-    strcpy(p2.fable->name,"Dummy");
-    p2.fable->lane=1;
 
-    if(SDL_Init(SDL_INIT_VIDEO)!=0){
-        printf("SDL_Init Error: %s\n",SDL_GetError());
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
-    SDL_Window *win=SDL_CreateWindow("Twisted Fables",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WSCREEN,HSCREEN,0);
-    if(!win){
-        printf("SDL_CreateWindow Error: %s\n",SDL_GetError());
+
+    SDL_Window *win = SDL_CreateWindow("Twisted Fables",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WSCREEN, HSCREEN, 0);
+    if(!win) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
-    SDL_Renderer *ren=SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
-    if(!ren){
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if(!ren) {
         SDL_DestroyWindow(win);
         SDL_Quit();
         return 1;
     }
-    while(Playing){
-        TitleScreen(ren,font);
-        BattleScreen(ren,&p1,&p2,font);
+
+    // Main game loop
+    while(Playing) {
+        TitleScreen(ren, font);
+        BattleScreen(ren, &p1, &p2, font);
     }
+
+    // Cleanup
     TTF_CloseFont(font);
     TTF_Quit();
     SDL_DestroyRenderer(ren);
