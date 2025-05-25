8160c035c4b3f62a77aff4b86f9074f023a745af
Reo <catatangerine@gmail.com>
2025-05-26 00:03:29 +0900

    Create declare.h

---
+++ declare.h
@@ -0,0 +1,6 @@
+#ifndef DECLARE_H_INCLUDED
+#define DECLARE_H_INCLUDED
+#include "base.h"
+void battle_setup(Player *p1, Player *p2,int starting);
+void BattleLane(SDL_Renderer *ren,Player *p1, Player *p2);
+#endif
