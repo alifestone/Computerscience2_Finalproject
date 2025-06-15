3c96914e0195bdaecfb53e48b7a84779cc669067
Reo <catatangerine@gmail.com>
2025-06-15 12:54:00 +0900

    Update red_hood.h

--- red_hood.h
+++ red_hood.h
@@ -1,5 +1,5 @@
-#ifndef DECLARE_H_INCLUDED
-#define DECLARE_H_INCLUDED
+#ifndef RED_HOOD_H_INCLUDED
+#define RED_HOOD_H_INCLUDED
 #include "base.h"
 void battle_setup(Player *p1, Player *p2,int starting);
 void BattleLane(SDL_Renderer *ren,Player *p1, Player *p2);
